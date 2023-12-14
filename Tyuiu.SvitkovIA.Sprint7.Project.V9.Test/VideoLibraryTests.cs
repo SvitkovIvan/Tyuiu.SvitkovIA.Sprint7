3bf89e33c9578be10c2192e4e617dbf75a5cb9e1
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-12-14 10:06:31 +0500

    Day3+test

--- Tyuiu.SvitkovIA.Sprint7.Project.V9.Test/VideoLibraryTests.cs
+++ Tyuiu.SvitkovIA.Sprint7.Project.V9.Test/VideoLibraryTests.cs
@@ -9,11 +9,24 @@ namespace Tyuiu.SvitkovIA.Sprint7.Project.V9.Test
 {
     public class VideoLibraryTests
     {
+        private const string TestCsvFilePath = "testVideoClips.csv";
+        private VideoLibrary videoLibrary;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Создаем новый экземпляр VideoLibrary и удаляем существующий тестовый CSV-файл (если есть)
+            videoLibrary = new VideoLibrary(TestCsvFilePath);
+            if (File.Exists(TestCsvFilePath))
+            {
+                File.Delete(TestCsvFilePath);
+            }
+        }
+
         [TestMethod]
         public void AddVideoClip_ValidData_VideoClipAdded()
         {
-            
-            VideoLibrary videoLibrary = new VideoLibrary("videoClips.csv");
+            // Arrange
             VideoClip videoClip = new VideoClip
             {
                 Code = "VC001",
@@ -30,24 +43,34 @@ namespace Tyuiu.SvitkovIA.Sprint7.Project.V9.Test
                 }
             };
 
-
+            // Act
             videoLibrary.AddVideoClip(videoClip);
 
-
+            // Assert
             Assert.AreEqual(1, videoLibrary.GetVideoClipsCount());
 
-
+            // Additional assertions (if needed)
             VideoClip addedVideoClip = videoLibrary.GetVideoClipByCode("VC001");
             Assert.IsNotNull(addedVideoClip);
             Assert.AreEqual("Action", addedVideoClip.Theme);
-
+            // ...
         }
 
         
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            
+            if (File.Exists(TestCsvFilePath))
+            {
+                File.Delete(TestCsvFilePath);
+            }
+        }
     }
-
-
 }
+
+        
     
 
 
