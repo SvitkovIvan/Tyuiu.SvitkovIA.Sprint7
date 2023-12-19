e7a778e8a2271aacab2d82533651977fcc90f0bf
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-12-19 22:42:24 +0500

    Day5+Test

---
+++ Tyuiu.SvitkovIA.Sprint7.Project.V9.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Tyuiu.SvitkovIA.Sprint7.Project.V9.Lib;
+
+namespace Tyuiu.SvitkovIA.Sprint7.Project.V9.Test
+{
+
+
+    [TestClass]
+    public class DataServiceTest
+    {
+        public void LoadFromDataFile()
+        {
+            string path = @"C:\DataSprint7\База Данных Видеолента.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
+
+
+
+
+
+
+
