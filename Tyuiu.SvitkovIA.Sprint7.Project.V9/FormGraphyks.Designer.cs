cf8918652d2a5d0d240783d6b9ce0539b5dd6673
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-12-19 23:25:35 +0500

    Day5+Forms

---
+++ Tyuiu.SvitkovIA.Sprint7.Project.V9/FormGraphyks.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Tyuiu.SvitkovIA.Sprint7.Project.V9
+{
+    partial class FormGraphyks_SIA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog_SIA = new System.Windows.Forms.OpenFileDialog();
+            this.toolStripMenuItemHelp_SIA = new System.Windows.Forms.Button();
+            this.toolStripMenuItemBack_SIA = new System.Windows.Forms.Button();
+            this.сохранитьToolStripMenuItemGraphyks_SIA = new System.Windows.Forms.Button();
+            this.saveFileDialog_SIA = new System.Windows.Forms.SaveFileDialog();
+            this.dataGridViewGraphyks_SIA = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGraphyks_SIA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog_SIA
+            // 
+            this.openFileDialog_SIA.FileName = "openFileDialog_SIA";
+            // 
+            // toolStripMenuItemHelp_SIA
+            // 
+            this.toolStripMenuItemHelp_SIA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.toolStripMenuItemHelp_SIA.Location = new System.Drawing.Point(608, 311);
+            this.toolStripMenuItemHelp_SIA.Name = "toolStripMenuItemHelp_SIA";
+            this.toolStripMenuItemHelp_SIA.Size = new System.Drawing.Size(100, 37);
+            this.toolStripMenuItemHelp_SIA.TabIndex = 0;
+            this.toolStripMenuItemHelp_SIA.Text = "Помощь";
+            this.toolStripMenuItemHelp_SIA.UseVisualStyleBackColor = false;
+            this.toolStripMenuItemHelp_SIA.Click += new System.EventHandler(this.toolStripMenuItemHelp_SIA_Click);
+            // 
+            // toolStripMenuItemBack_SIA
+            // 
+            this.toolStripMenuItemBack_SIA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.toolStripMenuItemBack_SIA.Location = new System.Drawing.Point(485, 311);
+            this.toolStripMenuItemBack_SIA.Name = "toolStripMenuItemBack_SIA";
+            this.toolStripMenuItemBack_SIA.Size = new System.Drawing.Size(95, 37);
+            this.toolStripMenuItemBack_SIA.TabIndex = 1;
+            this.toolStripMenuItemBack_SIA.Text = "Назад";
+            this.toolStripMenuItemBack_SIA.UseVisualStyleBackColor = false;
+            this.toolStripMenuItemBack_SIA.Click += new System.EventHandler(this.toolStripMenuItemBack_SIA_Click);
+            // 
+            // сохранитьToolStripMenuItemGraphyks_SIA
+            // 
+            this.сохранитьToolStripMenuItemGraphyks_SIA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.сохранитьToolStripMenuItemGraphyks_SIA.Location = new System.Drawing.Point(328, 311);
+            this.сохранитьToolStripMenuItemGraphyks_SIA.Name = "сохранитьToolStripMenuItemGraphyks_SIA";
+            this.сохранитьToolStripMenuItemGraphyks_SIA.Size = new System.Drawing.Size(103, 37);
+            this.сохранитьToolStripMenuItemGraphyks_SIA.TabIndex = 2;
+            this.сохранитьToolStripMenuItemGraphyks_SIA.Text = "Сохранить";
+            this.сохранитьToolStripMenuItemGraphyks_SIA.UseVisualStyleBackColor = false;
+            this.сохранитьToolStripMenuItemGraphyks_SIA.Click += new System.EventHandler(this.сохранитьToolStripMenuItemGraphyks_SIA_Click);
+            // 
+            // dataGridViewGraphyks_SIA
+            // 
+            this.dataGridViewGraphyks_SIA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGraphyks_SIA.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewGraphyks_SIA.Name = "dataGridViewGraphyks_SIA";
+            this.dataGridViewGraphyks_SIA.RowHeadersWidth = 51;
+            this.dataGridViewGraphyks_SIA.RowTemplate.Height = 24;
+            this.dataGridViewGraphyks_SIA.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewGraphyks_SIA.TabIndex = 3;
+            // 
+            // FormGraphyks_SIA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewGraphyks_SIA);
+            this.Controls.Add(this.сохранитьToolStripMenuItemGraphyks_SIA);
+            this.Controls.Add(this.toolStripMenuItemBack_SIA);
+            this.Controls.Add(this.toolStripMenuItemHelp_SIA);
+            this.Name = "FormGraphyks_SIA";
+            this.Text = "FormGraphyks";
+            this.Load += new System.EventHandler(this.FormGraphyks_SIA_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGraphyks_SIA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog_SIA;
+        private System.Windows.Forms.Button toolStripMenuItemHelp_SIA;
+        private System.Windows.Forms.Button toolStripMenuItemBack_SIA;
+        private System.Windows.Forms.Button сохранитьToolStripMenuItemGraphyks_SIA;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog_SIA;
+        private System.Windows.Forms.DataGridView dataGridViewGraphyks_SIA;
+    }
+}
---
+++ Tyuiu.SvitkovIA.Sprint7.Project.V9/FormGraphyks.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.SvitkovIA.Sprint7.Project.V9.Lib;
+
+namespace Tyuiu.SvitkovIA.Sprint7.Project.V9
+{
+    public partial class FormGraphyks_SIA : Form
+    {
+        public FormGraphyks_SIA()
+        {
+            InitializeComponent();
+            openFileDialog_SIA.Filter = "Значения, разделенные запятыми(*.csv)|*.csv|Всефайлы(*.*)|*.*";
+        }
+
+        private void toolStripMenuItemHelp_SIA_Click(object sender, EventArgs e)
+        {
+            FormAbout formAbout = new FormAbout();
+            formAbout.ShowDialog();
+        }
+
+        private void toolStripMenuItemBack_SIA_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormMain_SIA formMain = new FormMain_SIA();
+            formMain.Show();
+        }
+
+        private void FormGraphyks_SIA_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormMain_SIA formMain = new FormMain_SIA();
+            formMain.Show();
+        }
+
+        private void сохранитьToolStripMenuItemGraphyks_SIA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog_SIA.FileName = ".csv";
+                saveFileDialog_SIA.InitialDirectory = @":C";
+                saveFileDialog_SIA.ShowDialog();
+                string path = saveFileDialog_SIA.FileName;
+                FileInfo fileInfo = new FileInfo(path);
+                bool fileExists = fileInfo.Exists;
+                if (fileExists)
+                {
+                    File.Delete(path);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Файл не сохранен", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static string openFile;
+        static int rows;
+        static int columns;
+        DataService ds = new DataService();
+        private void buttonOpenFile_URI_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog_SIA.ShowDialog();
+                openFile = openFileDialog_SIA.FileName;
+
+                string[,] matrix = ds.LoadFromDataFile(openFile);
+                rows = matrix.GetLength(0);
+                columns = matrix.GetLength(1);
+                dataGridViewGraphyks_SIA.RowCount = 250;
+                dataGridViewGraphyks_SIA.ColumnCount = 50;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    dataGridViewGraphyks_SIA.Columns[i].Width = 135;
+                }
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        dataGridViewGraphyks_SIA.Rows[i].Cells[j].Value = matrix[i, j];
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Файл не выбран", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
+    
+
